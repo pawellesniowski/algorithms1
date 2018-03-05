1fd276233595babd195c7e3231eaf7ad3bfecd38
Pawel Lesniowski <pawel_lesniowski@epam.com>
2018-03-05 08:46:23 +0100

    reverse int with: parseInt(stringToParse) and Math.sign(number)

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    // My first solution
+    // var newN = n.toString().split('').reverse();
+    // if(newN[newN.length-1] === "-"){
+    //     newN.unshift('-');
+    //     newN.pop();
+    // }
+    // return Number(newN.join(''));
+
+    let reversed = n.toString().split('').reverse().join('')
+    return Math.sign(n) * parseInt(reversed);
+    
+}
 
 module.exports = reverseInt;
